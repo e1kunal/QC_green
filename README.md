e441ec5ceda565be62ebe570e22e39be4ee5f115
e1kunal <kunal.das@elucidata.io>
2023-12-11 12:49:26 +0530

    first

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# QC_green
-Testing
+## Instructions
+
+1. Clone this repository to your local machine:
+   ```bash
+   git clone https://github.com/your-username/your-repository.git
+   cd your-repository
---
+++ example.py
@@ -0,0 +1,18 @@
+import csv
+
+def add_and_generate_csv(num1, num2):
+    # Add two numbers
+    result = num1 + num2
+
+    # Save result to CSV file
+    with open('result.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Number 1', 'Number 2', 'Result']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Write header
+        writer.writeheader()
+
+        # Write data
+        writer.writerow({'Number 1': num1, 'Number 2': num2, 'Result': result})
+
+    return result
